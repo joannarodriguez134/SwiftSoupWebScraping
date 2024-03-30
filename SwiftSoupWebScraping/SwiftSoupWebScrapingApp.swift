83dfe6291c58fcec071b974d927b57c16c84c7fb
joannarodriguez134 <joannarodriguez134@gmail.com>
2024-03-30 03:22:04 -0500

    Initial Commit

---
+++ SwiftSoupWebScraping/SwiftSoupWebScrapingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSoupWebScrapingApp.swift
+//  SwiftSoupWebScraping
+//
+//  Created by Joanna Rodriguez on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSoupWebScrapingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
